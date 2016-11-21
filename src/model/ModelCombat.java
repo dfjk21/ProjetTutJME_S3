50facba937602563f15ba0e6491111388d2776ad
dfjk21 <valentin.fadloun@yahoo.fr>
2016-11-21 11:46:34 +0100

    Update ModelCombat.java

--- src/model/ModelCombat.java
+++ src/model/ModelCombat.java
@@ -1,1 +1,36 @@
+package model;
+import java.util.Random;
 
+/**
+ * Created by raphael on 11/11/16.
+ */
+
+public class ModelCombat {
+  
+    Random input = new Random();
+    final int CRITIQUE_BASE = 15;
+    ModelCombat(){
+      
+    }
+  
+    public void attaquePhy(ModelEntite perso){
+        int force = perso.getForce();
+        int plus = input.nextInt(force)/2;
+        int degats = 1;
+        if (plus <= force/4){
+          degats = force - plus;
+        }
+        else{
+          degats = force + plus;
+        }
+        if(degats == 0){
+          degats = 1;
+        }
+        
+        critique = input.nextInt(100);
+        if(critique <= CRITIQUE_BASE){
+          degats = degats + degats*0.5;
+        }
+        perteVie(degats);
+    }
+}
