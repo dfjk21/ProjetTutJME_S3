12be36d3a8c7facc5d3bfab6350a8e5a9e714a00
Raphaël Rousseau <raph.rousseau11@gmail.com>
2017-01-05 21:32:40 +0100

    Classe pour la narration de la version sans graphisme. Choix entre utilisation de hashmap ou fichier XML pour stocker les données.

---
+++ src/model/ModelDialogue.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by raphael on 05/01/17.
+ */
+public class ModelDialogue {
+     /*Stocké les dialogues, le fil narrative dans un hashmap : id+contenue ou plutot fichier xml+JDOM?*/
+}
